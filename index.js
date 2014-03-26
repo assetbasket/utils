4d583672a1b6140bef74859ee1cdc82909228ea7
Codey Whitt <codey.whitt@pearson.com>
2014-03-26 17:44:38 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * index.js - Module entry point.
+ *
+ * This is where you should instantiate the return for your module.
+ */
+module.exports = (function () {
+	console.info('Please implement something...');
+})()
